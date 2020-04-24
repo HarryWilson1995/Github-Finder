bde3289970f7af8a148628173a29a71d991a526d
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-04-24 10:41:28 -0400

    setting up fetch for repos data

--- github.js
+++ github.js
@@ -2,17 +2,25 @@ class GitHub {
   constructor() {
     this.client_id = "f486df3d0649f668256f";
     this.client_secret = "14a94cf209d5d1f097b1582dc3556c177f113a12";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
 
     const profile = await profileResponse.json();
 
+    const repos = await repoResponse.json();
+
     return {
       profile,
+      repos,
     };
   }
 }

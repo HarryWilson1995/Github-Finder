c8eec25563f88cfce1e0724858acf1849fb15e1d
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-04-24 10:28:42 -0400

    added clear profile function when user deletes input in search bar

--- ui.js
+++ ui.js
@@ -6,17 +6,17 @@ class UI {
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
       <div class="row">
-        <div class="col-md-3>
+        <div class="col-md-3">
           <img class="img-fluid mb-2" src="${user.avatar_url}">
-          <a href="${user.html_url}" target="_blank" class= "btn btn-primary btn-block">View Profile</a>
+          <a href="${user.html_url}" target="_blank" class= "btn btn-primary btn-block mb-4">View Profile</a>
         </div>
-        <div class="col-md-9>
+        <div class="col-md-9">
           <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
           <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
           <span class="badge badge-success">Followers: ${user.followers}</span>
           <span class="badge badge-info">Following: ${user.following}</span>
           <br><br>
-          <ul class="list-group>
+          <ul class="list-group">
             <li class="list-group-item">Company: ${user.company}</li>
             <li class="list-group-item">Website/Blog: ${user.blog}</li>
             <li class="list-group-item">Location: ${user.location}</li>
@@ -28,5 +28,9 @@ class UI {
     <h3 class="page-heading mb-3">Latest Repos</h3>
     <div id="repos"></div>
     `;
+  }
+
+  clearProfile() {
+    this.profile.innerHTML = "";
   }
 }
